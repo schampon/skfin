7e0173835913abd60bfb881a79a2840b5a81e4e3
Sylvain Champonnois <sylvain.champonnois@cfm.fr>
2023-04-11 10:21:41 +0200

    Add create env script

---
+++ create_env.sh
@@ -0,0 +1,6 @@
+conda create python=3.9 --name  skfin -c https://conda.anaconda.org/conda-forge/ -y
+conda activate skfin
+
+pip install -r requirements.txt
+pip install -e .
+python -m ipykernel install --user --name skfin --display-name "Python (skfin)"
